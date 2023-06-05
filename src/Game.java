52a815aa568316f5cf59598e5832c3d7aac73d23
Janis Gruettmueller <janis.gr@outlook.com>
2023-06-05 16:20:09 +0200

    implement methods for tetris game logic

--- src/Game.java
+++ src/Game.java
@@ -75,43 +75,124 @@ public class Game {
     }
 
     public void createNewPiece() {
-
+        Random rand = new Random();
+        currentPiece = new Piece(3, -2);
+        currentPiece.setShapeID(rand.nextInt(0, 7));
     }
 
 
     public void moveRight() {
+        Point currentPosition = currentPiece.getCurrentPosition();
 
+        if(!isCollision(currentPosition.x + 1, currentPosition.y, currentPiece.getRotation())) {
+            currentPiece.setCurrentXPosition(currentPosition.x + 1);
+        }
     }
 
     public void moveLeft() {
+        Point currentPosition = currentPiece.getCurrentPosition();
 
+        if(!isCollision(currentPosition.x - 1, currentPosition.y, currentPiece.getRotation())) {
+            currentPiece.setCurrentXPosition(currentPosition.x - 1);
+        }
     }
 
     public void moveDown() {
+        Point currentPosition = currentPiece.getCurrentPosition();
 
+        if(!isCollision(currentPosition.x, currentPosition.y + 1, currentPiece.getRotation())) {
+            currentPiece.setCurrentYPosition(currentPosition.y + 1);
+        }
     }
 
     public void rotate() {
+        Point currentPosition = currentPiece.getCurrentPosition();
+        int rotation = currentPiece.getRotation();
 
+        if(!isCollision(currentPosition.x + 1, currentPosition.y + 1, rotation) && !isCollision(currentPosition.x - 1, currentPosition.y + 1, rotation)) {
+            if(rotation == 3) {
+                currentPiece.setRotation(0);
+            } else {
+                currentPiece.setRotation(rotation+1);
+            }
+        }
     }
 
 
     public boolean isCollision(int xPos, int yPos, int rotation) {
+        for (Point p : tetraminos[currentPiece.getShapeID()][rotation]) {
+            // Piece collision
+            if(gridPoints.contains(new Point(p.x + xPos, p.y + yPos))) {
+                return true;
+            }
 
+            // Wall collision
+            if(xPos + p.x == BOARD_WIDTH || xPos + p.x < 0) {
+                return true;
+            }
+            if(yPos + p.y == BOARD_HEIGHT-1) {
+                return true;
+            }
+        }
 
         return false;
     }
 
     public void fixToGrid() {
+        for (Point p : tetraminos[currentPiece.getShapeID()][currentPiece.getRotation()]) {
+            Point newPoint = new Point(p.x + currentPiece.getCurrentPosition().x, p.y + currentPiece.getCurrentPosition().y);
+            // Game Over logik
+            if(newPoint.y <= 0) {
+                gameOver = true;
+            }
 
+            gridPoints.add(newPoint);
+        }
+
+        for (Point p : gridPoints) {
+            grid[p.y][p.x] = true;
+        }
+
+        clearRows();
     }
 
     public void clearRows() {
+        int numberOfRows = 0;
 
+        for (int i = 0; i < grid.length; i++) {
+            int counter = 0;
+            for (int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] == true) {
+                    counter++;
+                    grid[i][j] = false;
+                }
+            }
+
+            if (counter == grid[i].length) {
+                numberOfRows++;
+                deleteRow(i);
+            }
+        }
+
+        switch(numberOfRows) {
+            case 1 -> score += 100;
+            case 2 -> score += 300;
+            case 3 -> score += 500;
+            case 4 -> score += 800;
+        }
     }
 
     public void deleteRow(int row) {
+        for (int i = 0; i < grid[row].length; i++) {
+            gridPoints.remove(new Point(i, row));
+        }
 
+        for (Point p : gridPoints) {
+            if(p.y < row) {
+                grid[p.y][p.x] = false;
+                p.y += 1;
+            }
+        }
     }
 
 
