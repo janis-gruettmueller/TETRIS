b268932f56f673de2d4335215f413fb507f6caff
Janis Gruettmueller <janis.gr@outlook.com>
2023-06-05 16:07:21 +0200

    implemnt Piece class adding attributes + adding getter and setter methods

--- src/Piece.java
+++ src/Piece.java
@@ -1,2 +1,39 @@
+import java.awt.*;
+
 public class Piece {
+    private Point currentPosition;
+    private int rotation;
+    private int shapeID;
+
+    public Piece(int xPos, int yPos) {
+        this.currentPosition = new Point(xPos, yPos);
+        this.rotation = 0;
+    }
+
+
+    public void setRotation(int rotation) {
+        this.rotation = rotation;
+    }
+    public void setShapeID(int shapeID) {
+        this.shapeID = shapeID;
+    }
+
+
+    public void setCurrentXPosition(int xPos) {
+        this.currentPosition.x = xPos;
+    }
+    public void setCurrentYPosition(int yPos) {
+        this.currentPosition.y = yPos;
+    }
+
+
+    public int getRotation() {
+        return rotation;
+    }
+    public Point getCurrentPosition() {
+        return currentPosition;
+    }
+    public int getShapeID() {
+        return shapeID;
+    }
 }
